8e2905d21b747032d0f72e022b2afe2464edf026
Denis Onyshchuk <o.82.denis@gmail.com>
2021-03-24 14:15:21 +0200

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -14704,7 +14704,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "9775" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "10411" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
