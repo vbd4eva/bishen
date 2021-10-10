79ed95f22823a127d64765fd649aafd287465be1
Denis Onyshchuk <o.82.denis@gmail.com>
2021-10-10 22:45:37 +0300

    bubbly-button

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+document.querySelector('html').classList.remove('no-js');
+document.querySelector('html').classList.add('js');
+
 import './sass/main.scss';
 
 import './js/clients-slider/init';
+import './js/buttons/bubbly-button';
 
 // import './js/mobile-toogle/init';
 
